7a1796bbfc5bbae4f96cd47e8f6aa68a19606804
DongYoon Kang <canfrog@naver.com>
2022-07-27 15:42:04 +0900

    Practice input, output & four fundamental arithmetic operations

---
+++ 01_IO_Operation/1008.cpp
@@ -0,0 +1,17 @@
+// A / B
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double A, B;
+    cin >> A;
+    cin >> B;
+
+    cout.precision(9);
+    cout << fixed;
+    cout << A / B;
+
+    return 0;
+}
---
+++ 01_IO_Operation/10869.cpp
@@ -0,0 +1,19 @@
+// 사칙연산
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A;
+    cin >> B;
+
+    cout << A + B << endl;
+    cout << A - B << endl;
+    cout << A * B << endl;
+    cout << A / B << endl;
+    cout << A % B << endl;
+
+    return 0;
+}
