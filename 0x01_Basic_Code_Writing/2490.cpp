cbd96f6d32e2c504d2f55c0fcf63789775226214
Shineast <canfrog@naver.com>
2024-01-02 23:45:25 +0900

    0x01 (2)

---
+++ 0x01_Basic_Code_Writing/2490.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int result, input;
+string res = "DCBAE";
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    for (int j = 0; j < 3; j++) {
+        result = 0;
+        for (int i = 0; i < 4; i++) {
+            cin >> input;
+            result += input;
+        }
+        cout << res[result] << "\n";
+    }
+}
