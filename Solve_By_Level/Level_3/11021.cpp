cc7833bf8476ea5aaf86229c79e13283f4df86f3
Kangdy <canfrog@naver.com>
2024-03-09 23:29:45 +0900

    Solve Level 3 (2)

---
+++ Solve_By_Level/Level_3/11021.cpp
@@ -0,0 +1,17 @@
+// A + B - 7 / 브론즈 5
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); 
+    cin.tie(0); 
+
+    int test, A, B;
+    cin >> test;
+
+    for (int i = 1; i <= test; i++) {
+        cin >> A >> B;
+        cout << "Case #" << i << ": " << A + B << "\n";
+    }
+}
--- Solve_By_Level/Level_3/8393.cpp
+++ Solve_By_Level/Level_3/8393.cpp
@@ -7,7 +7,7 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n = 0;
+    int n;
     cin >> n;
     
     cout << n * (n+1) / 2 << "\n";
