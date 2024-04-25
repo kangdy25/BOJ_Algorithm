7cebbf3e707937382e4b2d4b3584e44dde86e01f
Kangdy <canfrog@naver.com>
2024-04-25 18:27:35 +0900

    Solve Level 9

---
+++ Solve_By_Level/Level_9/2501.cpp
@@ -0,0 +1,25 @@
+// 약수 구하기 / 브론즈 3
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N, K, count = 0;
+    cin >> N >> K;
+    
+    for (int i = 1; i <= N; i++) {
+        if (N % i == 0) {
+            count++;
+        }
+        if (count == K) {
+            cout << i;
+            break;
+        }
+    }
+    if (count < K) {
+        cout << "0";
+    }
+}
