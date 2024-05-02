15c81c03b921d184e66c2901432ea96a777668cd
Kangdy <canfrog@naver.com>
2024-05-02 23:06:14 +0900

    Solve Level 10 (1)

---
+++ Solve_By_Level/Level_10/15894.cpp
@@ -0,0 +1,14 @@
+// 수학은 체육과목 입니다 / 브론즈 3
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    long long N;
+    cin >> N;
+    
+    cout << 4 * N;
+}
