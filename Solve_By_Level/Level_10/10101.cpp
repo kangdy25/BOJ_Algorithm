c901b99ba03c4ca9f5c3fc0ce03847b48a654477
Kangdy <canfrog@naver.com>
2024-05-03 12:29:12 +0900

    Solve Level 10 (2)

---
+++ Solve_By_Level/Level_10/10101.cpp
@@ -0,0 +1,22 @@
+// 삼각형 외우기 / 브론즈 4
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int A, B, C;
+    cin >> A >> B >> C;
+    
+    if (A == 60 && B == 60 && C == 60) {
+        cout << "Equilateral";
+    } else if (A + B + C == 180 && (A == B || B == C || C == A)) {
+        cout << "Isosceles";
+    } else if (A + B + C == 180 && (A != B && B != C && C != A)) {
+        cout << "Scalene";
+    } else {
+        cout << "Error";
+    }
+}
---
+++ Solve_By_Level/Level_10/14215.cpp
@@ -0,0 +1,19 @@
+// 세 막대 / 브론즈 3
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    if ((a + b + c - max({a, b, c})) <= max({a, b, c})) {
+        cout << 2 * (a + b + c - max({a, b, c})) - 1;
+    } else {
+        cout << a + b + c;
+    }
+    
+}
